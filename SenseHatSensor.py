414389b7facd09acdd266ef1e229969ca1043798
Rob Culliton <rob.culliton@gmail.com>
2019-12-14 13:39:42 -0500

    round floats, split file logic into its own file for now

--- SenseHatSensor.py
+++ SenseHatSensor.py
@@ -5,22 +5,14 @@ sense = SenseHat()
 sense.clear()
 
 pressure = sense.get_pressure()
-print("Atmospheric pressure is", pressure)
+print("Atmospheric pressure is", round(pressure))
 
 humidity = sense.get_humidity()
 
-print("Relative humidity is", humidity)
-
+print("Relative humidity is", round(humidity))
 temp = sense.get_temperature()
 
-print("Temperature in Celcius is", temp)
+print("Temperature in Celcius is", round(temp))
 
 
 
-# ok to start let's get the current date, and then write to a date file
-# with recordings every ten minutes. then the next day we start with a new file :)
-
-f = open("2019-12-14.txt", "a")
-f.write("Current humidity is:")
-f.write(str(humidity))
-f.close()
