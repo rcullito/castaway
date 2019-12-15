a9351a21f05d89e373dcf3b77231a814358e2138
Rob Culliton <rob.culliton@gmail.com>
2019-12-14 21:10:17 -0500

    decently working ligature script

--- ligature.py
+++ ligature.py
@@ -8,6 +8,12 @@ from sense_hat import SenseHat
 def pretty_sense(sensory):
     return (str(round(sensory)))
 
+def make_timestamp(t):
+    return t.strftime("%X")
+
+def build_file_row(t, environmentals):
+    return make_timestamp(t) + " pressure, humidity, & temperature: " + environmentals
+
 # initialize sensor
 sense = SenseHat()
 sense.clear()
@@ -16,20 +22,22 @@ sense.clear()
 pressure = sense.get_pressure()
 humidity = sense.get_humidity()
 temperature = sense.get_temperature()
-environmentals = [pressure, humidity, temperature]
 
-print_worthy = ", ".join(map(pretty_sense, environmentals))
+#format environmental readings
+sensors = [pressure, humidity, temperature]
+print_worthy_sensors = ", ".join(map(pretty_sense, sensors))
 
-
+# flesh out some useful date info
 todays_date = date.today()
 date_filename = str(todays_date) + ".txt"
-
-f = open(date_filename, "a")
-
 t = datetime.now()
 
-timestamp_message = t.strftime("%X")
+# combine date and sense data into the message
+# we'll actually append to the file
+timestamp_message = build_file_row(t, print_worthy_sensors)
 
+# open today's file & write timestamped environmental data to it
+f = open(date_filename, "a")
 f.write(timestamp_message)
 f.close()
 
