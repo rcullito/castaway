1e990f05a1fd250beccbd603565b07a693fc638b
Rob Culliton <rob.culliton@gmail.com>
2019-12-14 20:18:23 -0500

    working date & time ligature file

--- dates.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-from datetime import date
-
-t = datetime.now()
-
-print("The current time of day is",
-      t.strftime("%X"))
-
-print("The current date is ")
--- ligature.py
+++ ligature.py
@@ -3,12 +3,17 @@
 from datetime import datetime
 from datetime import date
 
-todays_date = str(date.today()) + ".txt"
-
+todays_date = date.today()
+date_filename = str(todays_date) + ".txt"
 #TODO spoof this with a date we don't already
 # have to see how it will fail
-f = open(todays_date, "a")
-f.write("Somewhere between jazz and opera ")
+f = open(date_filename, "a")
+
+t = datetime.now()
+
+timestamp_message = "The current time of day is " + t.strftime("%X")
+
+f.write(timestamp_message)
 f.close()
 
 
