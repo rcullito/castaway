fc8457f95bc16ada716862f902ae2513f795051c
Rob Culliton <rob.culliton@gmail.com>
2019-12-14 21:24:24 -0500

    remove unused files

--- filestuff.py
+++
@@ -1,9 +0,0 @@
-# ok to start let's get the current date, and then write to a date file
-# with recordings every ten minutes. then the next day we start with a new file :)
-
-f = open("2019-12-14.txt", "a")
-f.write("Current humidity is:")
-f.write(str(humidity))
-f.close()
-
-# write a file if it doesn't already exist
--- syntax.py
+++
@@ -1,4 +0,0 @@
-# because I am learning python as we go
-
-def pretty_sense(sensory):
-    return (str(round(sensory)))
