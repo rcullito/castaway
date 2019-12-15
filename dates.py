d0fce4ba55b7cfaea440294fd4aef6336a6690a9
Rob Culliton <rob.culliton@gmail.com>
2019-12-14 20:12:01 -0500

    write to date file

--- dates.py
+++ dates.py
@@ -1,22 +1,9 @@
 from datetime import datetime
 from datetime import date
 
-today = datetime.now()
+t = datetime.now()
 
-print("The current date and time is", today)
-
-# just the time
-
-t = datetime.time(datetime.now())
-
-
-# here: just down to the minute is preferable
-print("The current time is", t)
-print("the current time, only to the second is",
+print("The current time of day is",
       t.strftime("%X"))
 
-print("The current date is", date.today())
-
-
-# open the file, date, and append the time
-# only ever append
+print("The current date is ")
--- ligature.py
+++ ligature.py
@@ -1,8 +1,16 @@
 # mishmosh of the other files
 # this may end up being the useful one
+from datetime import datetime
+from datetime import date
 
+todays_date = str(date.today()) + ".txt"
 
-f = open("2019-12-14.txt", "a")
-f.write("Current humidity as of" bleh " is:")
-f.write(str(humidity))
+#TODO spoof this with a date we don't already
+# have to see how it will fail
+f = open(todays_date, "a")
+f.write("Somewhere between jazz and opera ")
 f.close()
+
+
+
+
