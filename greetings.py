3c0187bb07b199f964560297f3ef759bc2dd6085
Rob Culliton <rob.culliton@gmail.com>
2019-12-31 20:07:05 -0500

    green message, and learning to clear

--- greetings.py
+++ greetings.py
@@ -3,6 +3,16 @@ sense = SenseHat()
 
 
 #.05 will scroll at twice the usual speed
-sense.show_message("Hello, World!", text_colour=(255, 255, 0),
-                   back_colour=(0,0,255),
-                   scroll_speed=(0.05))
+# bigger the number, slower the speed
+
+# red, green, blue
+#sense.show_message("Hello, World!", text_colour=(255, 255, 0),
+ #                  back_colour=(0,0,0),
+  #                 scroll_speed=(0.05))
+
+
+# success
+sense.show_message("Thanks", back_colour=(0,255,0))
+
+sense.clear(0, 0, 0)
+
